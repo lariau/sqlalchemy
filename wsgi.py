2f22a775f91278950923da5816872f811c0dbd75
christophe lariau <lariau@yahoo.fr>
2018-11-29 11:36:59 +0100

    deployment to heroku with pg

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,26 @@ try:
     load_dotenv()
 except:
     pass
+
 from flask import Flask
+from config import Config
+
 app = Flask(__name__)
+app.config.from_object(Config)
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+from models import Product
+from schemas import products_schema
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/api/v1/products')
+def products():
+    products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
+    return products_schema.jsonify(products)
